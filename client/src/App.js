8d452b9511ec86da0a3d1c1aad46912d032146d8
Jhansi Mandadi <jmandadi@nisum.com>
2023-06-07 14:01:15 +0530

    register page changes

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,28 @@ import AddMed from './AddMed';
 import Supply from './Supply'
 import Track from './Track'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 function App() {
   return (
     <div className="App">
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">WEB3.0</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/register">Register</Nav.Link>
+            <Nav.Link href="/addmed">Order Medicines</Nav.Link>
+            <Nav.Link href="/supply">Control Supply Chain</Nav.Link>
+            <Nav.Link href="/track">Track Medicines</Nav.Link>
+
+          </Nav>
+        </Container>
+      </Navbar>
       <Router>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/roles" component={AssignRoles} />
+          <Route path="/register" component={AssignRoles} />
           <Route path="/addmed" component={AddMed} />
           <Route path="/supply" component={Supply} />
           <Route path="/track" component={Track} />
--- client/src/AssignRoles.js
+++ client/src/AssignRoles.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Web3 from "web3";
 import SupplyChainABI from "./artifacts/SupplyChain.json"
-import { useHistory } from "react-router-dom"
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Table from 'react-bootstrap/Table';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
 
 function AssignRoles() {
-    const history = useHistory()
     useEffect(() => {
         loadWeb3();
         loadBlockchaindata();
@@ -12,22 +14,14 @@ function AssignRoles() {
     const [currentaccount, setCurrentaccount] = useState("");
     const [loader, setloader] = useState(true);
     const [SupplyChain, setSupplyChain] = useState();
-    const [RMSname, setRMSname] = useState();
-    const [MANname, setMANname] = useState();
-    const [DISname, setDISname] = useState();
-    const [RETname, setRETname] = useState();
-    const [RMSplace, setRMSplace] = useState();
-    const [MANplace, setMANplace] = useState();
-    const [DISplace, setDISplace] = useState();
-    const [RETplace, setRETplace] = useState();
-    const [RMSaddress, setRMSaddress] = useState();
-    const [MANaddress, setMANaddress] = useState();
-    const [DISaddress, setDISaddress] = useState();
-    const [RETaddress, setRETaddress] = useState();
-    const [RMS, setRMS] = useState();
-    const [MAN, setMAN] = useState();
-    const [DIS, setDIS] = useState();
-    const [RET, setRET] = useState();
+    const [tableData, setTableData] = useState({})
+    const [address, setAddress] = useState();
+    const [name, setName] = useState();
+    const [place, setPlace] = useState();
+    const [addMethod, setAddMethod] = useState();
+    const [subTitle, setSubTitle] = useState("");
+    const [showMain, setShowMain] = useState(true);
+
 
     const loadWeb3 = async () => {
         if (window.ethereum) {
@@ -53,256 +47,120 @@ function AssignRoles() {
         if (networkData) {
             const supplychain = new web3.eth.Contract(SupplyChainABI.abi, networkData.address);
             setSupplyChain(supplychain);
-            var i;
-            const rmsCtr = await supplychain.methods.rmsCtr().call();
-            const rms = {};
-            for (i = 0; i < rmsCtr; i++) {
-                rms[i] = await supplychain.methods.RMS(i + 1).call();
-            }
-            setRMS(rms);
-            const manCtr = await supplychain.methods.manCtr().call();
-            const man = {};
-            for (i = 0; i < manCtr; i++) {
-                man[i] = await supplychain.methods.MAN(i + 1).call();
-            }
-            setMAN(man);
-            const disCtr = await supplychain.methods.disCtr().call();
-            const dis = {};
-            for (i = 0; i < disCtr; i++) {
-                dis[i] = await supplychain.methods.DIS(i + 1).call();
-            }
-            setDIS(dis);
-            const retCtr = await supplychain.methods.retCtr().call();
-            const ret = {};
-            for (i = 0; i < retCtr; i++) {
-                ret[i] = await supplychain.methods.RET(i + 1).call();
-            }
-            setRET(ret);
             setloader(false);
-        }
-        else {
-            window.alert('The smart contract is not deployed to current network')
-        }
+        } else window.alert('The smart contract is not deployed to current network')
     }
+
     if (loader) {
         return (
             <div>
                 <h1 className="wait">Loading...</h1>
             </div>
         )
-
-    }
-    const redirect_to_home = () => {
-        history.push('/')
-    }
-    const handlerChangeAddressRMS = (event) => {
-        setRMSaddress(event.target.value);
-    }
-    const handlerChangePlaceRMS = (event) => {
-        setRMSplace(event.target.value);
-    }
-    const handlerChangeNameRMS = (event) => {
-        setRMSname(event.target.value);
-    }
-    const handlerChangeAddressMAN = (event) => {
-        setMANaddress(event.target.value);
-    }
-    const handlerChangePlaceMAN = (event) => {
-        setMANplace(event.target.value);
-    }
-    const handlerChangeNameMAN = (event) => {
-        setMANname(event.target.value);
-    }
-    const handlerChangeAddressDIS = (event) => {
-        setDISaddress(event.target.value);
-    }
-    const handlerChangePlaceDIS = (event) => {
-        setDISplace(event.target.value);
-    }
-    const handlerChangeNameDIS = (event) => {
-        setDISname(event.target.value);
-    }
-    const handlerChangeAddressRET = (event) => {
-        setRETaddress(event.target.value);
-    }
-    const handlerChangePlaceRET = (event) => {
-        setRETplace(event.target.value);
-    }
-    const handlerChangeNameRET = (event) => {
-        setRETname(event.target.value);
-    }
-    const handlerSubmitRMS = async (event) => {
-        event.preventDefault();
-        try {
-            var reciept = await SupplyChain.methods.addRMS(RMSaddress, RMSname, RMSplace).send({ from: currentaccount });
-            if (reciept) {
-                loadBlockchaindata();
-            }
-        }
-        catch (err) {
-            alert("An error occured!!!")
-        }
-    }
-    const handlerSubmitMAN = async (event) => {
-        event.preventDefault();
-        try {
-            var reciept = await SupplyChain.methods.addManufacturer(MANaddress, MANname, MANplace).send({ from: currentaccount });
-            if (reciept) {
-                loadBlockchaindata();
-            }
-        }
-        catch (err) {
-            alert("An error occured!!!")
-        }
-    }
-    const handlerSubmitDIS = async (event) => {
-        event.preventDefault();
-        try {
-            var reciept = await SupplyChain.methods.addDistributor(DISaddress, DISname, DISplace).send({ from: currentaccount });
-            if (reciept) {
-                loadBlockchaindata();
-            }
-        }
-        catch (err) {
-            alert("An error occured!!!")
-        }
-    }
-    const handlerSubmitRET = async (event) => {
-        event.preventDefault();
-        try {
-            var reciept = await SupplyChain.methods.addRetailer(RETaddress, RETname, RETplace).send({ from: currentaccount });
-            if (reciept) {
-                loadBlockchaindata();
-            }
-        }
-        catch (err) {
-            alert("An error occured!!!")
-        }
     }
 
+    const cilickOnCard = async (ctrl, type, addMethod, title) => {
+        setAddMethod(addMethod);
+        setSubTitle(title);
+        setShowMain(false)
+        var i;
+        const controls = await SupplyChain.methods?.[ctrl]().call();
+        const records = {};
+        for (i = 0; i < controls; i++) {
+            records[i] = await SupplyChain.methods?.[type](i + 1).call();
+        }
+        setTableData(records)
+    }
+
+    const handlerChangePlace = (event) => {
+        setPlace(event.target.value);
+    }
+
+    const handlerChangeName = (event) => {
+        setName(event.target.value);
+    }
+
+    const handlerChangeAddress = (event) => {
+        setAddress(event.target.value);
+    }
+    const handlerSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            var reciept = await SupplyChain.methods?.[addMethod](address, name, place).send({ from: currentaccount });
+            if (reciept) {
+                loadBlockchaindata();
+            }
+        }
+        catch (err) {
+            alert("An error occured!!!")
+        }
+    }
 
 
     return (
-        <div>
-            <span><b>Current Account Address:</b> {currentaccount}</span>
-            <span onClick={redirect_to_home} className="btn btn-outline-danger btn-sm">HOME</span>
-            <h4>Raw Material Suppliers:</h4>
-            <form onSubmit={handlerSubmitRMS}>
-                <input className="form-control-sm" type="text" onChange={handlerChangeAddressRMS} placeholder="Ethereum Address" required />
-                <input className="form-control-sm" type="text" onChange={handlerChangeNameRMS} placeholder="Raw Material Supplier Name" required />
-                <input className="form-control-sm" type="text" onChange={handlerChangePlaceRMS} placeholder="Based In" required />
-                <button className="btn btn-outline-success btn-sm" onSubmit={handlerSubmitRMS}>Register</button>
-            </form>
-            <table className="table table-sm">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Place</th>
-                        <th scope="col">Ethereum Address</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {Object.keys(RMS).map(function (key) {
-                        return (
-                            <tr key={key}>
-                                <td>{RMS[key].id}</td>
-                                <td>{RMS[key].name}</td>
-                                <td>{RMS[key].place}</td>
-                                <td>{RMS[key].addr}</td>
+        <>
+            {showMain ? <CardGroup>
+                <Card onClick={() => cilickOnCard("rmsCtr", "RMS", "addRMS", "Raw Material Suppliers")}>
+                    <Card.Img variant="top" src="https://www.opscentre.com/wp-content/uploads/2017/11/bigstock-Supplyer-111025163.jpg" />
+                    <Card.Body>
+                        <Card.Title>Raw Material Suppliers</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card onClick={() => cilickOnCard("manCtr", "MAN", "addMAN", "Manufacturers")}>
+                    <Card.Img variant="top" src="https://media.istockphoto.com/id/1184804468/photo/industrial-technology-concept-factory-automation-smart-factory-industry-4-0.jpg?s=612x612&w=0&k=20&c=1MaCUFJnqZmuugNhMyL5kt4q0BMwiNpzmnJbSggBE6I=" />
+                    <Card.Body>
+                        <Card.Title>Manufacturers</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card onClick={() => cilickOnCard("disCtr", "DIS", "addDIS", "Distributors")}>
+                    <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1osa1d35nNiqI7ry5kfRpP9d3GBmNXczgvchuEuD1UOGv8nz0Tr_J3PUQLRYxe9f6HYU&usqp=CAU" />
+                    <Card.Body>
+                        <Card.Title>Distributors</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card onClick={() => cilickOnCard("retCtr", "RET", "addRET", "Retailers")}>
+                    <Card.Img variant="top" src="https://topfranchise.com/upload/resize_cache/webp/upload/medialibrary/a63/a63b8282cd62a05600431d1c1992094d.webp" />
+                    <Card.Body>
+                        <Card.Title>Retailers</Card.Title>
+                    </Card.Body>
+                </Card>
+            </CardGroup> :
+                <>
+                    <Breadcrumb>
+                        <Breadcrumb.Item onClick={() => setShowMain(true)}>List</Breadcrumb.Item>
+                        <Breadcrumb.Item active>{subTitle}</Breadcrumb.Item>
+                        <Breadcrumb.Item active>Add</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <form onSubmit={handlerSubmit}>
+                        <input className="form-control-sm m-r-15" type="text" onChange={handlerChangeAddress} placeholder="Ethereum Address" required />
+                        <input className="form-control-sm m-r-15" type="text" onChange={handlerChangeName} placeholder="Raw Material Supplier Name" required />
+                        <input className="form-control-sm m-r-15" type="text" onChange={handlerChangePlace} placeholder="Based In" required />
+                        <button className="btn btn-outline-success btn-sm" onSubmit={handlerSubmit}>Add</button>
+                    </form>
+                    <Table responsive="sm">
+                        <thead>
+                            <tr>
+                                <th>S. No</th>
+                                <th>Name</th>
+                                <th>Place</th>
+                                <th>Ethereum Address</th>
                             </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-            <h4>Manufacturers:</h4>
-            <form onSubmit={handlerSubmitMAN}>
-                <input className="form-control-sm" type="text" onChange={handlerChangeAddressMAN} placeholder="Ethereum Address" required />
-                <input className="form-control-sm" type="text" onChange={handlerChangeNameMAN} placeholder="Manufacturer Name" required />
-                <input className="form-control-sm" type="text" onChange={handlerChangePlaceMAN} placeholder="Based In" required />
-                <button className="btn btn-outline-success btn-sm" onSubmit={handlerSubmitMAN}>Register</button>
-            </form>
-            <table className="table table-sm">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Place</th>
-                        <th scope="col">Ethereum Address</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {Object.keys(MAN).map(function (key) {
-                        return (
-                            <tr key={key}>
-                                <td>{MAN[key].id}</td>
-                                <td>{MAN[key].name}</td>
-                                <td>{MAN[key].place}</td>
-                                <td>{MAN[key].addr}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-            <h4>Distributors:</h4>
-            <form onSubmit={handlerSubmitDIS}>
-                <input className="form-control-sm" type="text" onChange={handlerChangeAddressDIS} placeholder="Ethereum Address" required />
-                <input className="form-control-sm" type="text" onChange={handlerChangeNameDIS} placeholder="Distributor Name" required />
-                <input className="form-control-sm" type="text" onChange={handlerChangePlaceDIS} placeholder="Based In" required />
-                <button className="btn btn-outline-success btn-sm" onSubmit={handlerSubmitDIS}>Register</button>
-            </form>
-            <table className="table table-sm">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Place</th>
-                        <th scope="col">Ethereum Address</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {Object.keys(DIS).map(function (key) {
-                        return (
-                            <tr key={key}>
-                                <td>{DIS[key].id}</td>
-                                <td>{DIS[key].name}</td>
-                                <td>{DIS[key].place}</td>
-                                <td>{DIS[key].addr}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-            <h4>Retailers:</h4>
-            <form onSubmit={handlerSubmitRET}>
-                <input className="form-control-sm" type="text" onChange={handlerChangeAddressRET} placeholder="Ethereum Address" required />
-                <input className="form-control-sm" type="text" onChange={handlerChangeNameRET} placeholder="Retailer Name" required />
-                <input className="form-control-sm" type="text" onChange={handlerChangePlaceRET} placeholder="Based In" required />
-                <button className="btn btn-outline-success btn-sm" onSubmit={handlerSubmitRET}>Register</button>
-            </form>
-            <table className="table table-sm">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Place</th>
-                        <th scope="col">Ethereum Address</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {Object.keys(RET).map(function (key) {
-                        return (
-                            <tr key={key}>
-                                <td>{RET[key].id}</td>
-                                <td>{RET[key].name}</td>
-                                <td>{RET[key].place}</td>
-                                <td>{RET[key].addr}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-        </div>
+                        </thead>
+                        <tbody>
+                            {Object.keys(tableData).map(function (key) {
+                                return (
+                                    <tr key={key}>
+                                        <td>{tableData[key]?.id}</td>
+                                        <td>{tableData[key]?.name}</td>
+                                        <td>{tableData[key]?.place}</td>
+                                        <td>{tableData[key]?.addr}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </Table>
+                </>}
+        </>
     )
 }
 

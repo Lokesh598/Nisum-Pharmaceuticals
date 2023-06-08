ea8114055a54af85ee3cd5e588e528af73992fd9
Jhansi Mandadi <jmandadi@nisum.com>
2023-06-08 17:26:57 +0530

    changes

--- client/src/Home.js
+++ client/src/Home.js
@@ -4,7 +4,10 @@ function Home() {
 
     return (
         <div className="homebg">
-            <h3>Pharmaceutical Supply Chain Flow :-- </h3>
+            <div className="project-title"> <h1>Pharmaceutical Supply Chain</h1>
+                <span>Responsibilities include oversight of all aspects of Pharmacy purchasing including medication procurement, inventory management, contracting, and participation in and compliance with the 3408 Federal Drug Discount Program for all covered entities.</span>
+
+            </div>
         </div>
     )
 }

2412f028aa58839004f2e78c2e5c72ac07776b5d
Jhansi Mandadi <jmandadi@nisum.com>
2023-06-09 10:13:48 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -11,15 +11,21 @@ import Nav from 'react-bootstrap/Nav';
 function App() {
   return (
     <div className="App">
-      <Navbar bg={window?.location?.pathname === "/" ?"transparent" : "primary"} variant="dark">
+      <Navbar bg={window?.location?.pathname === "/" ? "transparent" : "primary"} variant="dark">
         <Container>
-          <Navbar.Brand href="/">WEB3.0</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img
+              alt=""
+              src="https://lever-client-logos.s3.us-west-2.amazonaws.com/d4ee0bc6-7ccb-4762-86e1-07d7a007314b-1645085353318.png"
+              height="36"
+              className="d-inline-block align-top"
+            /> WEB3.0
+          </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/register">Register</Nav.Link>
-            <Nav.Link href="/addmed">Order Medicines</Nav.Link>
-            <Nav.Link href="/supply">Control Supply Chain</Nav.Link>
-            <Nav.Link href="/track">Track Medicines</Nav.Link>
-
+            <Nav.Link href="/register" className={window?.location?.pathname === "/register" ? "active-link" : ""}>Register</Nav.Link>
+            <Nav.Link href="/order-medicine" className={window?.location?.pathname === "/order-medicine" ? "active-link" : ""}>Order Medicines</Nav.Link>
+            <Nav.Link href="/control-supply-chain" className={window?.location?.pathname === "/control-supply-chain" ? "active-link" : ""}>Control Supply Chain</Nav.Link>
+            <Nav.Link href="/track-medicine" className={window?.location?.pathname === "/track-medicine" ? "active-link" : ""}>Track Medicines</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -27,9 +33,9 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/register" component={AssignRoles} />
-          <Route path="/addmed" component={AddMed} />
-          <Route path="/supply" component={Supply} />
-          <Route path="/track" component={Track} />
+          <Route path="/order-medicine" component={AddMed} />
+          <Route path="/control-supply-chain" component={Supply} />
+          <Route path="/track-medicine" component={Track} />
         </Switch>
       </Router>
     </div>
